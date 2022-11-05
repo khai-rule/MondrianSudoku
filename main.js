a2e788a12e27daea709f1ee3bbca4f478c621f8e
Khairul <Qai-rule@hotmail.com>
2022-11-05 21:34:17 +0800

    user can toggle on/off grid outline

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
 import $ from "jquery";
 
 const game = {
-  numbers: [1,2,3,4,5,6,7,8,9],
-  insert: 0
+  insert: 0,
+  outline: 0
 };
 
 
 
-
-// const enterNumber = () => {
-//   $(".sudoku-tile").on("click", () => {
-//     $(".sudoku.tile").text("Hi")
-//   })
-// }
-
-
-// enterNumber()
-
-
-
-
-
-// Board
+//! Board
 const $createBoard = () => {
   for (let i = 0; i < 9; i++) {
     const $section = $("<div>").addClass("sudoku-section");
@@ -34,18 +20,47 @@ const $createBoard = () => {
 
 $createBoard();
 
-// Number Buttons
+
+
+//! Hide/Remove Tiles Outline
+const $createOutline = () => {
+  const $outline = $("<button>").attr("id", "outline").text("Outline")
+  $(".buttons").append($outline);
+  $("#outline").on("click", () => {
+    if (game.outline === 0) {
+      game.outline = 1;
+      $render();
+    } else if (game.outline === 1) {
+      game.outline = 0;
+      $render()
+    }
+  })
+}
+
+$createOutline()
+
+
+
+//! Number Buttons
 const $createNum = () => {
   for (let i = 1; i < 10; i++) {
     const $num = $("<button>").addClass("numbers").text(i);
-    $(".number-buttons").append($num);
+    $(".buttons").append($num);
 }}
 
 $createNum();
 
-
+//! Render
 const $render = (event) => {
     $(event).text(game.insert);
+    if (game.outline === 1) {
+      $(".sudoku-tile").css("outline", "solid 1px white")
+      $(".sudoku-section").css("outline", "solid 2px white")
+    } else {
+      $(".sudoku-tile").css("outline", "none")
+      $(".sudoku-section").css("outline", "none")
+    }
+    
 }
 
 

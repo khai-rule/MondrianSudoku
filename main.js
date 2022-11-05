2ac80b785d3a480ffb031adf83c3b7c39b1ce691
Khairul <Qai-rule@hotmail.com>
2022-11-05 19:01:58 +0800

    click a number to fill the tiles with that number

--- main.js
+++ main.js
@@ -1,8 +1,64 @@
 import $ from "jquery";
 
-const numbers = [];
-
-const render = () => {
-
+const game = {
+  numbers: [1,2,3,4,5,6,7,8,9],
+  insert: 0
 };
 
+
+
+
+// const enterNumber = () => {
+//   $(".sudoku-tile").on("click", () => {
+//     $(".sudoku.tile").text("Hi")
+//   })
+// }
+
+
+// enterNumber()
+
+
+
+
+
+// Board
+const $createBoard = () => {
+  for (let i = 0; i < 9; i++) {
+    const $section = $("<div>").addClass("sudoku-section");
+    $(".board").append($section);
+  } for (let i = 0; i < 9; i++) {
+    const $tiles = $("<div>").addClass("sudoku-tile");
+    $(".sudoku-section").append($tiles)
+  }
+}
+
+$createBoard();
+
+// Number Buttons
+const $createNum = () => {
+  for (let i = 1; i < 10; i++) {
+    const $num = $("<button>").addClass("numbers").text(i);
+    $(".number-buttons").append($num);
+}}
+
+$createNum();
+
+
+const $render = () => {
+    $(".sudoku-tile").text(game.insert);
+    console.log(1)
+}
+
+
+const $insert = () => {
+  for (let i = 1; i <= 9; i++) {
+    $(".numbers").eq(i-1).on("click", () => {
+      game.insert = i;
+      
+      $render()
+    })
+    }
+  }
+
+
+$insert()

ba79db123708d997fb6887f6f69a661ef3297ebd
Khairul <Qai-rule@hotmail.com>
2022-11-06 16:27:52 +0800

    reconstruct board to give unique id to each box

--- main.js
+++ main.js
@@ -3,8 +3,18 @@ import $ from "jquery";
 const game = {
   insert: 0,
   outline: 0,
-  levels: ["Easy", "Medium", "Hard"]
+  levels: ["Easy", "Medium", "Hard"],
+  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 };
+
+
+//! New Game
+const $newGame = () => {
+  const $new = $("<button>").attr("id", "new-game").text("New Game")
+  $("#control").append($new)
+}
+$newGame();
+
 
 
 //! Difficulty Level
@@ -20,34 +30,44 @@ $difficultyLevel()
 
 
 //! Board
+// const $createBoard = () => {
+//   for (let i = 0; i < 9; i++) {
+//     const $section = $("<div>").addClass("sudoku-section");
+//     $(".board").append($section);
+//   } for (let i = 0; i < 9; i++) {
+//     const $tiles = $("<div>").addClass("sudoku-tile");
+//     $(".sudoku-section").append($tiles)
+//   }
+// }
+
+//? Board with unique grid
 const $createBoard = () => {
-  for (let i = 0; i < 9; i++) {
-    const $section = $("<div>").addClass("sudoku-section");
-    $(".board").append($section);
-  } for (let i = 0; i < 9; i++) {
-    const $tiles = $("<div>").addClass("sudoku-tile");
-    $(".sudoku-section").append($tiles)
+  for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+      const $tiles = $("<div>").attr("id", `${i}-${j}`).addClass("tile");
+      $(".board").append($tiles)
+    }
   }
 }
 
 $createBoard();
 
-//! Hide/Remove Tiles Outline
-const $createOutline = () => {
-  const $outline = $("<button>").attr("id", "outline").text("Outline")
-  $(".buttons").append($outline);
-  $("#outline").on("click", () => {
-    if (game.outline === 0) {
-      game.outline = 1;
-      $render();
-    } else if (game.outline === 1) {
-      game.outline = 0;
-      $render()
-    }
-  })
-}
+// //! Hide/Remove Tiles Outline
+// const $createOutline = () => {
+//   const $outline = $("<button>").attr("id", "outline").text("Outline")
+//   $(".buttons").append($outline);
+//   $("#outline").on("click", () => {
+//     if (game.outline === 0) {
+//       game.outline = 1;
+//       $render();
+//     } else if (game.outline === 1) {
+//       game.outline = 0;
+//       $render()
+//     }
+//   })
+// }
 
-$createOutline()
+// $createOutline()
 
 
 
@@ -86,6 +106,16 @@ const $insert = () => {
     })
     }
   }
-
-
 $insert()
+
+
+
+//! Generate Numbers
+const $game = () => {
+  $("#new-game").on("click", () => {
+    for (let i = 0; i < 4; i++) {
+      $(".sudoku-tile").eq(Math.floor(Math.random()*9)).text(game.numbers[Math.floor(Math.random()*9)])
+    }
+  })
+}
+$game()

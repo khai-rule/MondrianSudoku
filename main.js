fedf757038e91cf5a2ecc0391a8e3efdb335c8d6
Khairul <Qai-rule@hotmail.com>
2022-11-07 23:00:11 +0800

    add more puzzles under easy

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-import $ from "jquery";
+import $, { removeData } from "jquery";
 
 const game = {
   insert: 0,
   outline: 0,
   levels: ["Easy", "Medium", "Hard"],
   numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  pages: ["start", "game", "pause", "details", "scoreboard"]
+  pages: ["start", "game", "pause", "details", "scoreboard"],
+  puzzle: 0
 };
 
 
+
 const easy = [
-  "6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------",
-  "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
+  [
+    "6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------",
+    "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
+  ],
+  [
+    "--9-------4----6-758-31----15--4-36-------4-8----9-------75----3-------1--2--3--",
+    "619472583243985617587316924158247369926531478734698152891754236365829741472163895"
+  ],
+  [
+    "-1-5-------97-42----5----7-5---3---7-6--2-41---8--5---1-4------2-3-----9-7----8--",
+    "712583694639714258845269173521436987367928415498175326184697532253841769976352841"
+  ],
 ];
 
-
+// Random Number to generate puzzle
+// const randomNum = Math.floor(Math.random()*easy.length)
 
 //! New Game
 const $newGame = () => {
@@ -72,24 +85,69 @@ const $createNum = () => {
 
 $createNum();
 
-//! Render
-const $render = (event) => {
-  const $completed = []
-    $(event).text(game.insert);
-    for (let i = 0; i < 81; i++) {
-      const x = $(`#${i+1}`).text();
-      $completed.push(x)
-      console.log($completed.join(""))
-    }
-    if (game.outline === 1) {
-      $(".tile").css("outline", "solid 1px white")
-    } else {
-      $(".tile").css("outline", "none")
-    }
-    // Compare
-    if ( $completed.join("") === easy[1]) {
-      console.log("nice") }
+//! Generate Random Number 
+const randomNum = () => {
+  const num = Math.floor(Math.random()*easy.length)
+  game.puzzle = num
+  return game.puzzle
+  }
+
+
+
+//! Clear Previous
+const $reset = () => {
+  for (let i = 0; i < 81; i++) {
+    $(`#${i+1}`).text("")
+  }
 }
+
+//TODO To hide
+//! Solve Game
+const $solveGame = () => {
+  const $solve = $("<button>").attr("id", "solve").text("Solve")
+  $("#control").append($solve)
+  // $("#solve").on("click", () => {
+  //   const completeNum = easy[randomNum][1].split("");
+  //   for (let i = 0; i < 80; i++) {
+  //     const num = easy[randomNum][1].split("");
+  //     $(`#${i+1}`).text(num[i]);
+  //     $(`#${i+1}`).css("pointer-events", "none")
+  //   }
+  // })
+}
+$solveGame()
+
+
+//! Generate Puzzle
+const $game = () => {
+  $("#new-game").on("click", () => {
+    $reset()
+    randomNum()
+    console.log(game.puzzle)
+    game.puzzle = randomNum()
+    console.log(`Puzzle ${game.puzzle}`)
+    const completeNum = easy[game.puzzle][0].split("");
+    for (let i = 0; i < 81; i++) {
+      const num = easy[game.puzzle][0].split("");
+      if (num[i] === "-") {
+        continue
+      }
+      $(`#${i+1}`).text(num[i]);
+      $(`#${i+1}`).css("pointer-events", "none")
+    }
+  })
+  $("#solve").on("click", () => {
+    const completeNum = easy[game.puzzle][1].split("");
+    for (let i = 0; i < 80; i++) {
+      const num = easy[game.puzzle][1].split("");
+      $(`#${i+1}`).text(num[i]);
+      $(`#${i+1}`).css("pointer-events", "none")
+    }
+  })
+}
+$game()
+
+
 
 //! Append Number
 const $insert = () => {
@@ -99,47 +157,30 @@ const $insert = () => {
       $(".tile").on("click", (event) => {
         $render(event.currentTarget)
       })  
-      
     })
     }
   }
 $insert()
 
-//! Generate preset numbers (incompleted)
-const $game = () => {
-  $("#new-game").on("click", () => {
-    const completeNum = easy[0].split("");
-    for (let i = 0; i < 81; i++) {
-      const num = easy[0].split("");
-      if (num[i] === "-") {
-        continue
-      }
-      $(`#${i+1}`).text(num[i]);
-      $(`#${i+1}`).css("pointer-events", "none")
-    }
-  })
+//! Render
+const $render = (event) => {
+  const $completed = []
+  // Add selected number to tile
+  $(event).text(game.insert);
+  // Compare
+  for (let i = 0; i < 81; i++) {
+    const x = $(`#${i+1}`).text();
+    $completed.push(x)
+    console.log($completed.join(""))
+  } if ($completed.join("") === easy[game.puzzle][1]) {
+    console.log("You Win!") }
+  // Toggle grid
+  if (game.outline === 1) {
+    $(".tile").css("outline", "solid 1px white")
+  } else {
+    $(".tile").css("outline", "none")
+  }
 }
-$game()
-
-
-//TODO To hide
-//! Solve Game
-const $solveGame = () => {
-  const $solve = $("<button>").attr("id", "solve").text("Solve")
-  $("#control").append($solve)
-  $("#solve").on("click", () => {
-    const completeNum = easy[1].split("");
-    for (let i = 0; i < 80; i++) {
-      const num = easy[1].split("");
-      $(`#${i+1}`).text(num[i]);
-      $(`#${i+1}`).css("pointer-events", "none")
-    }
-  })
-}
-$solveGame()
-
-
-
 
 //! Timer
 
@@ -153,8 +194,8 @@ let millisecond = 0;
 let cron;
 
 $("#new-game").on("click", () => start());
-document.form_main.pause.onclick = () => pause();
-document.form_main.reset.onclick = () => reset();
+// document.form_main.pause.onclick = () => pause();
+// document.form_main.reset.onclick = () => reset();
 
 function start() {
   pause();

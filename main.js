1a9dc251cf0d46e7d1d85d7c413551820ac0da9e
Khairul <Qai-rule@hotmail.com>
2022-11-07 10:53:18 +0800

    generate preset puzzle

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ const game = {
 };
 
 
+const easy = [
+  "6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------",
+  "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
+];
+
+
+
 //! New Game
 const $newGame = () => {
   const $new = $("<button>").attr("id", "new-game").text("New Game")
@@ -42,34 +49,30 @@ $difficultyLevel()
 
 //? Board with unique grid
 const $createBoard = () => {
-  for (let i = 1; i <= 9; i++) {
-    for (let j = 1; j <= 9; j++) {
-      const $tiles = $("<div>").attr("id", `${i}-${j}`).addClass("tile");
-      $(".board").append($tiles)
-    }
+  for (let i = 1; i <= 81; i++) {
+    const $tiles = $("<div>").attr("id", `${i}`).addClass("tile");
+    $(".board").append($tiles)
   }
 }
 
 $createBoard();
 
 // //! Hide/Remove Tiles Outline
-// const $createOutline = () => {
-//   const $outline = $("<button>").attr("id", "outline").text("Outline")
-//   $(".buttons").append($outline);
-//   $("#outline").on("click", () => {
-//     if (game.outline === 0) {
-//       game.outline = 1;
-//       $render();
-//     } else if (game.outline === 1) {
-//       game.outline = 0;
-//       $render()
-//     }
-//   })
-// }
+const $createOutline = () => {
+  const $outline = $("<button>").attr("id", "outline").text("Outline")
+  $(".buttons").append($outline);
+  $("#outline").on("click", () => {
+    if (game.outline === 0) {
+      game.outline = 1;
+      $render();
+    } else if (game.outline === 1) {
+      game.outline = 0;
+      $render()
+    }
+  })
+}
 
-// $createOutline()
-
-
+$createOutline()
 
 
 
@@ -86,11 +89,9 @@ $createNum();
 const $render = (event) => {
     $(event).text(game.insert);
     if (game.outline === 1) {
-      $(".sudoku-tile").css("outline", "solid 1px white")
-      $(".sudoku-section").css("outline", "solid 2px white")
+      $(".tile").css("outline", "solid 1px white")
     } else {
-      $(".sudoku-tile").css("outline", "none")
-      $(".sudoku-section").css("outline", "none")
+      $(".tile").css("outline", "none")
     }
     
 }
@@ -100,7 +101,7 @@ const $insert = () => {
   for (let i = 1; i <= 9; i++) {
     $(".numbers").eq(i-1).on("click", () => {
       game.insert = i;
-      $(".sudoku-tile").on("click", (event) => {
+      $(".tile").on("click", (event) => {
         $render(event.currentTarget)
       })
     })
@@ -111,11 +112,57 @@ $insert()
 
 
 //! Generate Numbers
+// const $game = () => {
+//   $("#new-game").on("click", () => {
+//     for (let i = 0; i < 4; i++) {
+//       $(".sudoku-tile").eq(Math.floor(Math.random()*9)).text(game.numbers[Math.floor(Math.random()*9)])
+//     }
+//   })
+// }
+// $game()
+
+
+// //! Generate random number to random tiles
+// const $game = () => {
+//   $("#new-game").on("click", () => {
+//     for (let i = 1; i <= 9; i++) {
+//         $(`#${Math.floor(Math.random()*81)}`).text(game.numbers[i]);
+//     }
+//   })
+// }
+
+// $game()
+
+
+// //! Generate preset numbers (completed)
+// const $game = () => {
+//   $("#new-game").on("click", () => {
+//     for (let i = 0; i < 81; i++) {
+//       const num = easy[1].split("");
+//       $(`#${i+1}`).text(num[i]);
+//     }
+//   })
+// }
+// $game()
+
+//! Generate preset numbers (incompleted)
 const $game = () => {
   $("#new-game").on("click", () => {
-    for (let i = 0; i < 4; i++) {
-      $(".sudoku-tile").eq(Math.floor(Math.random()*9)).text(game.numbers[Math.floor(Math.random()*9)])
+    for (let i = 0; i < 81; i++) {
+      const num = easy[0].split("");
+      if (num[i] === "-") {
+        
+        continue
+      }
+      $(`#${i+1}`).text(num[i]);
+      $(`#${i+1}`).css("pointer-events", "none")
     }
   })
 }
 $game()
+
+
+
+
+
+
